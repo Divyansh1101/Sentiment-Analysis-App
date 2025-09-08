ee4875a1eb44d9d589a8d89b14ead5a9bb43f97a
Divyansh Saxena <divyanshsaxena905@gmail.com>
2025-09-08 19:02:35 +0530

    Add files via upload

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,1 +1,9 @@
-a
+from streamlit_app import simple_clean, tokenize_and_remove_stopwords
+
+def test_simple_clean():
+    assert simple_clean(" Hello!!! ") == "Hello"
+
+def test_tokenize_and_stopwords():
+    toks = tokenize_and_remove_stopwords("This is a simple test sentence.")
+    assert isinstance(toks, list)
+    assert "simple" in [t.lower() for t in toks]
